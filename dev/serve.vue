a850af2b48a4b5f486726adb6feb40539831a3c5
David Dutkiewicz <dawid.dutkiewicz@gmail.com>
2022-04-06 16:36:32 +0200

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { V2pCommonSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  V2pCommonSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v2p-common-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as V2pCommonSample } from './v2p-common-sample.vue';
